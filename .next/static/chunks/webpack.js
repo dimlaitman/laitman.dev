ccadbddccf202414c68622d3156c05ddb7e9b00d
Dim Laitman <my@creation.vision>
2022-02-13 13:22:26 +0800

    fix error 2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0d36693c526659e4"; }
+/******/ 		__webpack_require__.h = function() { return "a1ef58ab1152bfcf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
