d3fb17ed74444065c1a891aa46816e70520c10bb
Dim Laitman <my@creation.vision>
2022-02-13 12:07:39 +0800

    .gitignore Fixed

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d69492a66d44945c"; }
+/******/ 		__webpack_require__.h = function() { return "5e8111ee334f8001"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
